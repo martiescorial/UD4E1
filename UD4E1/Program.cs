39bdd4de142d36e086db97de741db5768f305fc0
Martina <martinaescorial9@gmail.com>
2021-06-15 21:00:11 +0200

    Agregar archivos de proyecto.

---
+++ UD4E1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UD4E1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 8;
+            Console.WriteLine("A = {0}", A);
+            int B = 6;
+            Console.WriteLine("B = {0}", B);
+            int C = 2;
+            Console.WriteLine("C = {0}", C);
+            int N = A+B+C;
+            Console.WriteLine("N = {0}", N);
+        }
+    }
+}
